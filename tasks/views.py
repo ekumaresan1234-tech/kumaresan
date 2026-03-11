15a8748c2b44c68e917f119531696d2b3ece4032
kumaresan <kumaresan@2000>
2026-03-11 12:56:22 +0530

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+
+    return render(request, 'tasks/index.html', {'tasks': tasks, 'form': form})
+
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+
+    return render(request, 'tasks/update.html', {'form': form})
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('task_list')
